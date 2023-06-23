f141447ba7b9aaa52c8c5771253a88ed70c37e87
Vanessa Soares <vanis.sbs@gmail.com>
2023-06-23 03:59:54 -0300

    Apostila05(RN Intro): Exercício Listagem contatos - Primeira versão do componente App garantindo a funcionalidade

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react' 
+import { 
+  StyleSheet, 
+  Text, 
+  TextInput,
+  View, 
+  Button 
+} from 'react-native';
+
 
 export default function App() {
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [contatos, setContatos] = useState([{name: 'Joao',
+      tel: '35625458'  
+  }, {name: 'Joao',
+  tel: '35625458'  
+}]);
+  
+  const capturarNome = (nomeDigitado) => {
+    setNome(nomeDigitado)}
+
+  const capturarTel = (telDigitado) => {
+      setTelefone(telDigitado)}
+
+  
+  const adicionarContato = () =>  {
+    const teste = {name: nome, tel: telefone}
+    const aux = [...contatos, teste];
+    setContatos(aux);
+    console.log(contatos)
+  }
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.baseText}>
+        CONTATOS
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={capturarNome}
+        placeholder="Nome"
+        value={nome}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={capturarTel}
+        value={telefone}
+        placeholder="Telefone"
+        keyboardType="numeric"
+      />
+
+      <Button
+        title="Salvar Contato"
+        onPress={adicionarContato}
+      />
     </View>
   );
 }
@@ -16,5 +65,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'blue',
   },
-});
+  baseText: {
+    fontWeight: 'bold'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
+})
